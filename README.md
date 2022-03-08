e9ef753a7b01e31ff53e4395f32b57c7e49600cb
Heejun <ysyang706@gmail.com>
2022-03-08 10:22:30 +0900

    카잉달력 수정

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # algorithm-study
+알고리즘 공부중입니다.
--- 카잉달력/solution.py
+++ 카잉달력/solution.py
@@ -1,20 +1,23 @@
 import sys
-sys.stdin = open('./ex1.txt')
 
-def check_year(m,n,x,y):
+sys.stdin = open("./ex1.txt")
+
+
+def check_year(m, n, x, y):
     x -= 1
     y -= 1
     state = False
     k = x
-    while k < n*m:
+    while k < n * m:
         if k % n == y:
-            return k+1
+            return k + 1
             break
         k += m
     else:
         return -1
 
+
 t = int(sys.stdin.readline())
 for _ in range(t):
     m, n, x, y = list(map(int, sys.stdin.readline().split()))
-    print(check_year(m,n,x,y))
+    print(check_year(m, n, x, y))
